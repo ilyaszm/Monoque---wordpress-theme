9d6468c1f118590cdc5191cb7df232eeca14dff0
ilyas zemrani <ilyas.gkeeper@gmail.com>
2018-11-24 10:34:44 +0000

    creating custom columns

--- assets/inc/custom-post-type.php
+++ assets/inc/custom-post-type.php
@@ -11,7 +11,11 @@
 
 $contact = get_option( 'activate_contact' );
 if( $contact == 1 ) {
+
     add_action( 'init', 'monoque_contact_custom_post_type' );
+    add_filter( 'manage_monoque-contact_posts_columns', 'monoque_set_contact_columns' );
+    add_action( 'manage_monoque-contact_posts_custom_column', 'monoque_contact_custom_column', 10, 2 );
+
 }
 
 // Contact custom post type
@@ -36,6 +40,30 @@ function monoque_contact_custom_post_type()
     );
 
     register_post_type( 'monoque-contact', $args );
+}
+
+function monoque_set_contact_columns($columns)
+{
+    $newCol = array();
+    $newCol['title'] = 'Full Name';
+    $newCol['message'] = 'Message';
+    $newCol['email'] = 'Email';
+    $newCol['date'] = 'Date';
+    return $newCol;
+}
+
+function monoque_contact_custom_column($column, $post_id)
+{
+    switch($column)
+    {
+        case 'message':
+            echo get_the_excerpt();
+            break;
+        case 'email':
+            //
+            break;
+        
+    }
 }
 
 ?>
